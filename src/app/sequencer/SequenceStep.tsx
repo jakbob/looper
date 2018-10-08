0412c0e8a83eb1ecd5160220a2b3a3d2688cd95f
Jakob Florell <jakob.florell@valtech.se>
2018-10-08 10:57:08 +0200

    ✨  Input custom tone

--- src/app/sequencer/SequenceStep.tsx
+++ src/app/sequencer/SequenceStep.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames';
 import * as React from 'react';
+import ReactDOM = require('react-dom');
 import * as styles from './SequenceStep.scss';
 
 const notes = [
@@ -13,6 +14,9 @@ const notes = [
   'B4',
 ];
 
+const validKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+const validOctaves = ['1', '2', '3', '4', '5', '6', '7'];
+
 export interface Props {
   step: string;
   isActive: boolean;
@@ -20,15 +24,45 @@ export interface Props {
 }
 
 export class SequenceStep extends React.Component<Props, {}> {
+  private inputRef = React.createRef<HTMLInputElement>();
   public render() {
     return (
       <div 
         className={classnames(styles.step, {
           [styles.active]: this.props.isActive
         })}
-        onClick={() => this.incrementStep()}
-      >{this.props.step}</div>
+      >
+        <input 
+          ref={this.inputRef}
+          value={this.props.step}
+          className={styles.stepInput}
+        />
+      </div>
     )
+  }
+  
+  public componentDidMount() {
+    const input = ReactDOM.findDOMNode(this.inputRef.current);
+    input.addEventListener('keypress', this.handleInput);
+  }
+
+  public componentWillUnmount() {
+    const input = ReactDOM.findDOMNode(this.inputRef.current);
+    input.removeEventListener('keypress', this.handleInput);
+  }
+
+  private handleInput = (e: KeyboardEvent) => {
+    e.preventDefault();
+    const value = e.key.toUpperCase();
+
+    if (validKeys.some(tone => value === tone)) {
+      this.props.onStepChange(value + 4);
+      return;
+    }
+    if (validOctaves.some(oct => value === oct)) { 
+      this.props.onStepChange(this.props.step.slice(0, 1) + value);
+      return;
+    }
   }
 
   private incrementStep() {
